e0b7dc4b151b540947b7ef908c68be5fddf1afd2
Dylan Thorwaldson <dtthor@Dylanns-MacBook-Pro.local>
2018-06-09 00:24:07 -0500

    Convert movie poster project to React components

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Movie from './components/Movie'
+import MovieList from './components/MovieList'
+
+const App = () => (
+  <div className="tc code">
+    <MovieList />
+  </div>
+)
+
+export default App
---
+++ src/components/Movie.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Movie = ({ title, genre, id, poster }) =>
+   (
+    <div className='tc grow br3 pa3 ma2 dib bw2 shadow-5 bg-black-10'>
+      <img alt='poster' src={poster} />
+      <div>
+        <h3>{title}</h3>
+        <p className='f7'>{genre}</p>
+      </div>
+    </div>
+  )
+
+
+export default Movie
