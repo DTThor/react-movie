92e8ec3a025d1d15c4551973abcb99c99e6b8ecb
Dylan Thorwaldson <dtthor@Dylanns-MacBook-Pro.local>
2018-06-11 14:17:51 -0500

    Change searchfield to searchField

--- src/App.js
+++ src/App.js
@@ -9,29 +9,29 @@ class App extends Component {
     super()
     this.state = {
       movies,
-      searchfield: ''
+      searchField: ''
     }
     this.onSearchChange = this.onSearchChange.bind(this)
   }
 
   onSearchChange(e){
-    this.setState({ searchfield: e.target.value })
+    this.setState({ searchField: e.target.value })
   }
 
-  filterMoviesBy({ movies, searchfield }){
+  filterMoviesBy({ movies, searchField }){
     return movies.filter(movie =>
-       movie.title.toLowerCase().includes(searchfield.toLowerCase())
+       movie.title.toLowerCase().includes(searchField.toLowerCase())
     )
   }
 
 
 
   render(){
-    const {movies, searchfield} = this.state
+    const {movies, searchField} = this.state
     return (
       <div className="tc code">
         <SearchBar searchChange={this.onSearchChange}/>
-        <MovieList movies={this.filterMoviesBy({movies, searchfield})}/>
+        <MovieList movies={this.filterMoviesBy({movies, searchField})}/>
       </div>
     )
   }
