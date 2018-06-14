79031f19e7b3c38b42cf1f7163f733ee8f393ca0
Dylan Thorwaldson <dtthor@Dylanns-MacBook-Pro.local>
2018-06-14 10:56:21 -0500

    style search history and searchbar and add conditional history message

--- src/components/RecentSearches.js
+++ src/components/RecentSearches.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 
 const RecentSearches = ({ history, onClick, clearHistory }) =>
   (
-    <div className='tc dib pa2 br3 ma0 bw2 shadow-2 bg-light-blue'>
-      <h3>Last 5 Searches</h3>
-      <button onClick={clearHistory}>Clear History</button>
+    <div className='tc pa1 br3 ma0 bw2 shadow-2 bg-light-blue'>
+      <h3 className='dib ph3'>{history.length===0 ? 'Search to start a History' : 'Search History'}</h3>
+      <button className='shadow-hover' onClick={clearHistory}>Clear History</button>
       <div>
         {
           history.map((title, i) => (
-            <p key={i} className='underline-hover shadow-hover' onClick={onClick}>{title}</p>
+            <p key={i} className='underline-hover shadow-hover dib ph2 mt-0' onClick={onClick}>{title}</p>
           ))
         }
-        </div>
+      </div>
       </div>
 
   )
